fd0bbf6e9981cad07ecf8b79f4f31397a0851350
Нуфтанчики <300225@mail.ru>
2022-01-14 22:55:21 +0300

    del app my_auth

--- apps/my_auth/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from . import models
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.AuthUser
-        fields = ('display_name', 'avatar',)
--- apps/my_auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from .endpoint import views
-
-
-urlpatterns = [
-    path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'}))
-
-]
